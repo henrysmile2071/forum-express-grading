bae03b523c212f2af2a5ef6da7c53432d541d852
henrysmile2071 <henrychen.chc@gmail.com>
2022-12-06 18:20:56 +0800

    feat: initialize api server

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const path = require('path')
 const express = require('express')
-const routes = require('./routes')
+const { pages } = require('./routes')
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
@@ -31,7 +31,7 @@ app.use((req, res, next) => {
   res.locals.user = getUser(req)
   next()
 })
-app.use(routes)
+app.use(pages)
 
 app.listen(port, () => {
   console.info(`Example app listening on port ${port}!`)
