652df7ca7475caf8a2975b9857c801017239aa35
henrysmile2071 <henrychen.chc@gmail.com>
2022-12-07 01:59:38 +0800

    feat: add admin-services

---
+++ services/admin-services.js
@@ -0,0 +1,13 @@
+const { Restaurant, Category } = require('./../models')
+const adminServices = {
+  getRestaurants: (req, callback) => {
+    return Restaurant.findAll({
+      raw: true,
+      nest: true,
+      include: [Category]
+    })
+      .then(restaurants => callback(null, { restaurants }))
+      .catch(err => callback(err))
+  }
+}
+module.exports = adminServices
