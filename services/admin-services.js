a46f08f3e888b05303787377f17e8c2426134a90
henrysmile2071 <henrychen.chc@gmail.com>
2022-12-07 10:33:23 +0800

    feat: add api error handler & delete restaurant api

--- services/admin-services.js
+++ services/admin-services.js
@@ -8,6 +8,19 @@ const adminServices = {
     })
       .then(restaurants => callback(null, { restaurants }))
       .catch(err => callback(err))
+  },
+  deleteRestaurant: (req, callback) => {
+    return Restaurant.findByPk(req.params.id)
+      .then(restaurant => {
+        if (!restaurant) {
+          const err = new Error("Restaurant didn't exist!")
+          err.status = 404
+          throw err
+        }
+        return restaurant.destroy()
+      })
+      .then(deletedRestaurant => callback(null, { restaurant: deletedRestaurant }))
+      .catch(err => callback(err))
   }
 }
 module.exports = adminServices
